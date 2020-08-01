d0ae5ba68c3b617cb0d59781116064cd4ac404fb
b7leung <b7leung@ucsd.edu>
2020-07-31 17:20:36 -0700

    updated nautilus prep

--- nautilus_prep.sh
+++ nautilus_prep.sh
@@ -1,12 +1,20 @@
+# prep datasets for nautilus jobs. This is supposed to be run in the base directory of the project
+# in the pod's home directory
 
-tar -xzvf 03001627.tar.gz
-rm 03001627.tar.gz
+mkdir data
+cd data
 
+mkdir ShapeNet
+cp /home/svcl-oowl/brandon/research/occ_uda/data/compressed_datasets/03001627.tar.gz ShapeNet
+tar -xzvf ShapeNet/03001627.tar.gz
+rm ShapeNet/03001627.tar.gz
+
+cp /home/svcl-oowl/brandon/research/occ_uda/data/compressed_datasets/pix3d_processed.tar.gz .
 tar -xzvf pix3d_processed.tar.gz
 rm pix3d_processed.tar.gz
 
+cp /home/svcl-oowl/brandon/research/occ_uda/data/compressed_datasets/target_domain.tar.gz .
 tar -xzvf target_domain.tar.gz
 rm target_domain.tar.gz
 
-mkdir ShapeNet
-mv 03001627.tar.gz ShapeNet
+cd ..

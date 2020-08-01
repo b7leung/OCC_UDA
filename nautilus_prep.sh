654c3f3d2ea4091af16f4b808efb9574c9c8878f
b7leung <b7leung@ucsd.edu>
2020-07-31 17:19:50 -0700

    added nautlius prep

---
+++ nautilus_prep.sh
@@ -0,0 +1,12 @@
+
+tar -xzvf 03001627.tar.gz
+rm 03001627.tar.gz
+
+tar -xzvf pix3d_processed.tar.gz
+rm pix3d_processed.tar.gz
+
+tar -xzvf target_domain.tar.gz
+rm target_domain.tar.gz
+
+mkdir ShapeNet
+mv 03001627.tar.gz ShapeNet
